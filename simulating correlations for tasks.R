02dcaff8bc4702e1f3abcb5c79c128d8d93cb1a0
Dorothy Bishop <dorothy.bishop@psy.ox.ac.uk>
2017-10-25 16:09:45 +0100

    Including loop to run repeatedly to estimate variation in correlations

--- simulating correlations for tasks.R
+++ simulating correlations for tasks.R
@@ -44,22 +44,38 @@ colnames(alltask)<-mylabel
 #mywts has one col per task; row 1 is front wt, row 2 is post wt, row 3 is error
 #these are set to sum to one.
 
-mywts<-matrix(c(.7,.7,.45,.4,.2,.1,
-                .2,.1,.45,.4,.7,.7,
-                .1,.2,.1,.2,.1,.2),byrow=TRUE,nrow=3)
+mywts<-matrix(c(.7,.7,.35,.35,0,0,
+                0,.0,.35,.35,.7,.7,
+                .3,.3,.3,.3,.3,.3),byrow=TRUE,nrow=3)
 
 #We now use these weights to simulate data for 2 runs with each task
 thiscol<-0 #zero the counter for columns
 for (j in 1:2){
 for (i in 1:6){
  thiscol<-thiscol+1 #increment to next column
- alltask[,thiscol]<-mywts[1,i]*frontL+mywts[2,i]*postL+mywts[3,i]*rnorm(bigN,0,0)
+ alltask[,thiscol]<-mywts[1,i]*frontL+mywts[2,i]*postL+mywts[3,i]*rnorm(bigN,0,1)
 }
 }
 
-nrun<-100 #N runs of simulation
-#create dataframe to hold all the correlations
-allr<-data.frame(matrix(rep(NA,nrun*144),nrow=nrun) )
+nrun<-1000 #N runs of simulation
+#create dataframe to hold all the correlations, so we can see how variable from run to run
+allr<-data.frame(matrix(rep(NA,nrun*66),nrow=nrun) )
+colnames(allr)<-c('A1.B1','A1.C1','A1.D1','A1.E1','A1.F1',
+                  'A1.A2','A1.B2','A1.C2','A1.D2','A1.E2','A1.F2',
+                  'B1.C1','B1.D1','B1.E1','B1.F1',
+                  'B1.A2','B1.B2','B1.C2','B1.D2','B1.E2','B1.F2',
+                  'C1.D1','C1.E1','C1.F1',
+                  'C1.A2','C1.B2','C1.C2','C1.D2','C1.E2','C1.F2',
+                  'D1.E1','D1.F1',
+                  'D1.A2','D1.B2','D1.C2','D1.D2','D1.E2','D1.F2',
+                  'E1.F1',
+                  'E1.A2','E1.B2','E1.C2','E1.D2','E1.E2','E1.F2',
+                  'F1.A2','F1.B2','F1.C2','F1.D2','F1.E2','F1.F2',
+                  'A2.B2','A2.C2','A2.D2','A2.E2','A2.F2',
+                  'B2.C2','B2.D2','B2.E2','B2.F2',
+                  'C2.D2','C2.E2','C2.F2',
+                  'D2.E2','D2.F2',
+                  'E2.F2')
 
 for (j in 1:nrun)
 {
@@ -72,7 +88,8 @@ alltask.N<-alltask[sample(bigN,myN),]
 mycorrs<-rcorr(as.matrix(alltask.N), type="spearman") 
 mycorrs<-mycorrs$r #we just want the correlations and not the p-values that rcorr generates
 mycorrs<-data.frame(mycorrs)
-allr[j,]<-unlist(mycorrs)
+allr[j,]<-c(mycorrs[1,2:12],mycorrs[2,3:12],mycorrs[3,4:12],mycorrs[4,5:12],mycorrs[5,6:12],
+            mycorrs[6,7:12],mycorrs[7,8:12],mycorrs[8,9:12],mycorrs[9,10:12],mycorrs[10,11:12],mycorrs[11,12])
 }
 
 #just save and plot the last one
@@ -91,4 +108,7 @@ axis(1, at=1:12, labels=mylabel)
 text(mycorrs[,1],mycorrs[,i],
      label=mylabel)
 }
+
+
+rmeans<-apply(allr,2,mean)
 

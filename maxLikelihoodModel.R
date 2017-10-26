1a973a405c4f239c422e2f9f578aab9a45a60df5
Dorothy Bishop <dorothy.bishop@psy.ox.ac.uk>
2017-10-26 12:15:45 +0100

    revised to work with our simulated data

--- maxLikelihoodModel.R
+++ maxLikelihoodModel.R
@@ -5,38 +5,36 @@
 #source('http://openmx.psyc.virginia.edu/getOpenMx.R')
 
 require(OpenMx)
-
-data(myFADataRaw) #this is sample dataset that comes with OpenMx
-names(myFADataRaw)
-
-oneFactorRaw <- myFADataRaw[,c("x1", "x2", "x3", "x4", "x5", "x6")]#select 6 vars to work with
-
+myrawfile<-"raw_N30_tested_on_4_err0.35_1factor.csv"
+my_raw<- read.csv(myrawfile, header=TRUE, sep=",")
+my_raw<-my_raw[,-1] #remove column 1 (subject ID)
 #The sample script gave this ready computed, but better to compute as cov(oneFactorRaw)
-oneFactorCov <- cov(oneFactorRaw)
+oneFactorCov <- cov(my_raw,use="pairwise.complete.obs")
 
-oneFactorMeans<-apply(oneFactorRaw,2,mean)
+oneFactorMeans<-apply(my_raw,2,mean,na.rm=TRUE)
 
-dataRaw      <- mxData( observed=oneFactorRaw, type="raw" )
+dataRaw      <- mxData( observed=my_raw, type="raw" )
 
 # residual variances
-resVars      <- mxPath( from=c("x1","x2","x3","x4","x5","x6"), arrows=2,
-                        free=TRUE, values=c(1,1,1,1,1,1),
-                        labels=c("e1","e2","e3","e4","e5","e6") )
+resVars      <- mxPath( from=c('A1','B1','C1','D1','E1','F1','A2','B2','C2','D2','E2','F2'), arrows=2,
+                        free=TRUE, values=c(1,1,1,1,1,1,1,1,1,1,1,1),
+                        labels=c("e1","e2","e3","e4","e5","e6","e1","e2","e3","e4","e5","e6") )
 # latent variance
-latVar       <- mxPath( from="F1", arrows=2,
-                        free=TRUE, values=1, labels ="varF1" )
+latVar       <- mxPath( from="X1", arrows=2,
+                        free=TRUE, values=1, labels ="varX1" )
 # factor loadings
-facLoads     <- mxPath( from="F1", to=c("x1","x2","x3","x4","x5","x6"), arrows=1,
-                        free=c(FALSE,TRUE,TRUE,TRUE,TRUE,TRUE), values=c(1,1,1,1,1,1),
-                        labels =c("l1","l2","l3","l4","l5","l6") )
+facLoads     <- mxPath( from="X1", to=c('A1','B1','C1','D1','E1','F1','A2','B2','C2','D2','E2','F2'), arrows=1,
+                        free=c(FALSE,TRUE,TRUE,TRUE,TRUE,TRUE,FALSE,TRUE,TRUE,TRUE,TRUE,TRUE), values=c(1,1,1,1,1,1,1,1,1,1,1,1),
+                        labels =c("l1","l2","l3","l4","l5","l6","l1","l2","l3","l4","l5","l6") )
 # means
-means        <- mxPath( from="one", to=c("x1","x2","x3","x4","x5","x6","F1"), arrows=1,
-                        free=c(T,T,T,T,T,T,FALSE), values=c(1,1,1,1,1,1,0),
-                        labels =c("meanx1","meanx2","meanx3",
-                                  "meanx4","meanx5","meanx6",NA) )
+means        <- mxPath( from="one", to=c('A1','B1','C1','D1','E1','F1','A2','B2','C2','D2','E2','F2','X1'), arrows=1,
+                        free=c(T,T,T,T,T,T,T,T,T,T,T,T,FALSE), values=c(1,1,1,1,1,1,1,1,1,1,1,1,0),
+                        labels =c("meanA","meanB","meanC",
+                                  "meanD","meanE","meanF","meanA","meanB","meanC",
+                                  "meanD","meanE","meanF",NA) )
 
 oneFactorModel <- mxModel("Common Factor Model Path Specification", type="RAM",
-                          manifestVars=c("x1","x2","x3","x4","x5","x6"), latentVars="F1",
+                          manifestVars=c('A1','B1','C1','D1','E1','F1','A2','B2','C2','D2','E2','F2'), latentVars="X1",
                           dataRaw, resVars, latVar, facLoads, means)
 
 oneFactorFit<-mxRun(oneFactorModel)
